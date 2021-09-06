b41b9e0687765336d829bb4e9ccdcd914a79151e
Tucker Barrett <tuckerbarrett@gmail.com>
2021-09-06 16:27:00 -0400

    Adds get by id route to products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   try {
     const productData = await Product.findAll({
-      include: { all: true, nested: true}
+      include: { all: true, nested: true }
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -18,9 +18,20 @@ router.get('/', async (req, res) => {
 
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: { all: true, nested: true }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
