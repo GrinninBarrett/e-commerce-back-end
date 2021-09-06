8dcb70f058090682ec32ec788ccbbe8058397836
Tucker Barrett <tuckerbarrett@gmail.com>
2021-09-06 16:51:37 -0400

    Fixes small mistakes in category and product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,6 +27,7 @@ router.get('/:id', async (req, res) => {
     });
     if (!productData) {
       res.status(404).json({ message: 'No product found with that id!' });
+      return;
     }
     res.status(200).json(productData);
   } catch (err) {
