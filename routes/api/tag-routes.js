b1f7ab02741012577a31b54de97ecaa5696f1e8a
Tucker Barrett <tuckerbarrett@gmail.com>
2021-09-09 16:56:04 -0400

    Adds put request to tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,13 +22,11 @@ router.get('/:id', async (req, res) => {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product, as: 'products' }]
     });
-
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with that id!'});
       return;
     }
     res.status(200).json(tagData);
-
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,12 +42,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  
 });
 
 module.exports = router;
